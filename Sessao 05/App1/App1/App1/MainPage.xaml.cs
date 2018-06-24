ab0abe008eec75671ff8ab1e5ae3305bb90d645e
maiconalvesdev <maiconalves@maiconalves.com.br>
2018-06-24 00:14:46 -0300

    app cep feito

---
+++ Sessao 05/App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.servico.modelo;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		    btnbusca.Clicked += BuscarCep;
+
+		}
+
+	    private void BuscarCep(object sender, EventArgs e)
+	    {
+	        string cep = busca.Text.Trim();
+	        if (isValidCep(cep))
+	        {
+	            Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep);
+
+	            textosaida.Text = "Endereço: " + end.localidade;
+	        }
+	    }
+
+	    private bool isValidCep(string cep)
+	    {
+	        if (cep.Length == 8) return true;
+	        DisplayAlert("ERRO", "Cep deve ser de 8 caracteres", "OK");
+	        return false;
+	    }
+	}
+}
---
+++ Sessao 05/App1/App1/App1/servico/modelo/ViaCepServico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace App1.servico.modelo
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEndereco = string.Format(EnderecoUrl, cep);
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEndereco);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+            return end;
+        } 
+    }
+}
